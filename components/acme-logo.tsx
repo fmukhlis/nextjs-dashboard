aa5d702923465853953b5a0b72969fb9f3401716
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-08-25 15:30:32 +0000

    feat: add app logo

---
+++ components/acme-logo.tsx
@@ -0,0 +1,13 @@
+import { Earth } from "lucide-react";
+import { luckiestGuy } from "@/components/ui/fonts";
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${luckiestGuy.className} flex flex-row items-center leading-none text-white`}
+    >
+      <Earth className="h-12 w-12 rotate-45" />
+      <p className="text-[44px] tracking-wider ml-3">Acme</p>
+    </div>
+  );
+}
