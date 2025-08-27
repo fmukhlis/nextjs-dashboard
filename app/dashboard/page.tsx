2209f5a6e3bb4d4b85157b1ab94ea241018bfaf5
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-08-27 10:17:24 +0000

    feat: add basic DashboardPage with chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
-export default function Dashboard() {
-  return <p>Dashboard Page</p>;
+import RevenueChart from "@/components/dashboard/revenue-chart";
+import { luckiestGuy } from "@/components/fonts";
+import { fetchRevenue } from "@/lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  return (
+    <main>
+      <h1
+        className={`${luckiestGuy.className} mb-4 text-xl md:text-2xl tracking-wide`}
+      >
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
