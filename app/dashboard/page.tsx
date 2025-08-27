1f0006c774185c2cac98db1b63a35805c1125d97
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-08-27 10:45:43 +0000

    feat: use LatestInvoices in DashboardPage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,20 @@
 import RevenueChart from "@/components/dashboard/revenue-chart";
-import { luckiestGuy } from "@/components/fonts";
-import { fetchRevenue } from "@/lib/data";
+import LatestInvoices from "@/components/dashboard/latest-invoices";
+import { robotoSlab } from "@/components/fonts";
+import { fetchLatestInvoices, fetchRevenue } from "@/lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
-      <h1
-        className={`${luckiestGuy.className} mb-4 text-xl md:text-2xl tracking-wide`}
-      >
+      <h1 className={`${robotoSlab.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
