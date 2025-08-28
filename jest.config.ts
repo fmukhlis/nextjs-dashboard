2de944bb6fb2a152f79882aa8c7f47814bc8b65e
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-08-28 09:04:42 +0000

    fix: escape braces chars in Jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
     "!<rootDir>/*.config.{js,ts}",
     "!<rootDir>/coverage/**",
     "!<rootDir>/*rc.{js,ts}",
-    "!**/(async)/**",
+    "!**/\\(async\\)/**",
     "!<rootDir>/components/placeholders/**",
   ],
 
