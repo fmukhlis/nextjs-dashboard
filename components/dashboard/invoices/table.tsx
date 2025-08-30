c4ab580ebec88a384828606bf9dfb924e564920e
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-08-30 17:56:13 +0000

    feat: add placeholder table row to prevent layout shift

--- components/dashboard/invoices/table.tsx
+++ components/dashboard/invoices/table.tsx
@@ -118,6 +118,38 @@ export default async function InvoicesTable({
                   </td>
                 </tr>
               ))}
+              {invoices.length < 6 &&
+                Array.from({ length: 6 - invoices.length }, (_, i) => (
+                  <tr
+                    key={`${i}`}
+                    className="w-full border-b border-gray-50 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                  >
+                    <td className="relative overflow-hidden whitespace-nowrap py-3 pl-6 pr-3">
+                      <div className="flex items-center gap-3">
+                        <div className="h-8 w-8 rounded-full bg-gray-50"></div>
+                        <div className="h-6 w-24 rounded bg-gray-50"></div>
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      <div className="h-6 w-32 rounded bg-gray-50"></div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      <div className="h-6 w-16 rounded bg-gray-50"></div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      <div className="h-6 w-16 rounded bg-gray-50"></div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      <div className="h-6 w-16 rounded bg-gray-50"></div>
+                    </td>
+                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                      <div className="flex justify-end gap-3">
+                        <div className="h-[38px] w-[38px] rounded bg-gray-50"></div>
+                        <div className="h-[38px] w-[38px] rounded bg-gray-50"></div>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
