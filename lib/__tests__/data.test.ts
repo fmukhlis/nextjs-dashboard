b3bdfbe25f5e0cd180af156f7fb5ac364dfded42
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-08-31 09:33:30 +0000

    test: add tests for fetchFilteredInvoices() and fetchInvoicesPages()

--- lib/__tests__/data.test.ts
+++ lib/__tests__/data.test.ts
@@ -1,4 +1,10 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../data";
+import {
+  fetchCardData,
+  fetchFilteredInvoices,
+  fetchInvoicesPages,
+  fetchLatestInvoices,
+  fetchRevenue,
+} from "../data";
 import { __mock__ } from "@neondatabase/serverless";
 import { formatCurrency } from "../utils";
 
@@ -90,10 +96,51 @@ describe("fetchCardData()", () => {
     });
   });
 
-  // it("returns");
-
   it("throws an error", async () => {
     __mock__.mockedNeon.mockRejectedValue("");
     await expect(fetchCardData()).rejects.toThrow("Failed to fetch card data.");
+  });
+});
+
+describe("fetchFilteredInvoices()", () => {
+  it("returns invoices", async () => {
+    const mockData = [
+      {
+        id: "1",
+        customer_id: "1",
+        name: "John",
+        email: "john@example.com",
+        image_url: "john.png",
+        date: "2023-09-10T00:00:00.000Z",
+        amount: 10000,
+        status: "paid",
+      },
+    ];
+    __mock__.mockedNeon.mockResolvedValue(mockData);
+    const invoices = await fetchFilteredInvoices("", 1);
+    expect(invoices).toEqual(mockData);
+  });
+
+  it("throws an error", async () => {
+    __mock__.mockedNeon.mockRejectedValue("");
+    await expect(fetchFilteredInvoices("", 1)).rejects.toThrow(
+      "Failed to fetch invoices.",
+    );
+  });
+});
+
+describe("fetchInvoicesPages()", () => {
+  it("returns total pages", async () => {
+    const mockData = [{ count: 1 }];
+    __mock__.mockedNeon.mockResolvedValue(mockData);
+    const totalPages = await fetchInvoicesPages("");
+    expect(totalPages).toEqual(mockData[0].count);
+  });
+
+  it("throws an error", async () => {
+    __mock__.mockedNeon.mockRejectedValue("");
+    await expect(fetchInvoicesPages("")).rejects.toThrow(
+      "Failed to fetch total number of invoices.",
+    );
   });
 });
