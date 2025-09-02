1207aa10e4ecd25e538da4a7986141805f47a1de
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-09-02 07:14:17 +0000

    chore: install storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -14,6 +17,7 @@ const eslintConfig = [
   {
     rules: { "@typescript-eslint/no-require-imports": "off" },
   },
+  ...storybook.configs["flat/recommended"]
 ];
 
 export default eslintConfig;
