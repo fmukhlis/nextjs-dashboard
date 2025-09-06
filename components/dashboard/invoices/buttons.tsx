c111eb7edd43d7cea7925c6b80a920243a66346c
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-09-06 07:54:14 +0000

    style: adjust invoice button styles

--- components/dashboard/invoices/buttons.tsx
+++ components/dashboard/invoices/buttons.tsx
@@ -7,7 +7,7 @@ export function CreateInvoice() {
       href="/dashboard/invoices/create"
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
-      <span className="hidden md:block">Create Invoice</span>{" "}
+      <span className="hidden md:block">Create Invoice</span>
       <Plus className="h-5 md:ml-4" />
     </Link>
   );
@@ -17,7 +17,7 @@ export function UpdateInvoice() {
   return (
     <Link
       href="/dashboard/invoices"
-      className="rounded-md border p-2 hover:bg-gray-100"
+      className="flex rounded-md border p-2 hover:bg-gray-100"
     >
       <span className="sr-only">Update Invoice</span>
       <Pencil className="h-5 w-5" />
