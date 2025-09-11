39b55a3ba0489a4e4ff0493e42b42b9f7efd425b
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-09-11 10:55:11 +0000

    test: add test for customers DAL and DTO

---
+++ data/__tests__/customers.test.ts
@@ -0,0 +1,45 @@
+import { __mock__ } from "@neondatabase/serverless";
+import { getCustomers } from "../customers";
+import { getCustomersDTO } from "../customers-dto";
+
+jest.mock("@neondatabase/serverless", () => {
+  const mockedNeon = jest.fn();
+  return {
+    __esModule: true,
+    neon: () => mockedNeon,
+    __mock__: { mockedNeon },
+  };
+});
+
+describe("Get Customers", () => {
+  it("returns data correctly", async () => {
+    const mockData = [
+      {
+        id: "1",
+        name: "John Doe",
+      },
+      {
+        id: "2",
+        name: "Alice",
+      },
+    ];
+    __mock__.mockedNeon.mockResolvedValue(mockData);
+
+    // DAL
+    const expectedDal = mockData;
+    const dal = await getCustomers();
+    expect(dal).toEqual(expectedDal);
+
+    // DTO
+    const expectedDto = expectedDal;
+    const dto = await getCustomersDTO();
+    expect(dto).toEqual(expectedDto);
+  });
+
+  it("throws an error", async () => {
+    __mock__.mockedNeon.mockRejectedValue("");
+    await expect(getCustomers()).rejects.toThrow(
+      "Failed to fetch all customers.",
+    );
+  });
+});
