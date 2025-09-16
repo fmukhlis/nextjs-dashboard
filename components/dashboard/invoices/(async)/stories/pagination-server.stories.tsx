ed235477bebe3c544ee75cd82ca4039d7a63eb13
Fajar Mukhlis Imananda <95970390+fmukhlis@users.noreply.github.com>
2025-09-16 22:30:08 +0700

    Update pagination-server.stories.tsx

--- components/dashboard/invoices/(async)/stories/pagination-server.stories.tsx
+++ components/dashboard/invoices/(async)/stories/pagination-server.stories.tsx
@@ -1,7 +1,9 @@
 import PaginationServer from "../pagination-server";
 
+import { mocked } from "storybook/test";
 import { Suspense } from "react";
 import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { getInvoicesPagesDTO } from "@/data/invoices-dto";
 import { PaginationServerSkeleton } from "@/components/dashboard/invoices/skeletons";
 
 const meta = {
@@ -11,6 +13,9 @@ const meta = {
     layout: "centered",
   },
   tags: ["autodocs"],
+  async beforeEach() {
+    mocked(getInvoicesPagesDTO).mockResolvedValue(9);
+  },
 } satisfies Meta<typeof PaginationServer>;
 
 export default meta;
