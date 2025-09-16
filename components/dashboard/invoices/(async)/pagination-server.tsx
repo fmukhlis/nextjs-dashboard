7343953cccb862645c565530e76f376bc3fa58bd
Fajar Mukhlis Imananda <95970390+fmukhlis@users.noreply.github.com>
2025-09-16 22:01:17 +0700

    Update pagination-server.tsx

--- components/dashboard/invoices/(async)/pagination-server.tsx
+++ components/dashboard/invoices/(async)/pagination-server.tsx
@@ -1,8 +1,9 @@
 import Pagination from "@/components/dashboard/invoices/pagination";
 
-import { getInvoicesPagesDTO } from "@/data/invoices-dto";
+// import { getInvoicesPagesDTO } from "@/data/invoices-dto";
 
 export default async function PaginationServer({ query }: { query: string }) {
-  const totalPages = await getInvoicesPagesDTO(query);
+  // const totalPages = await getInvoicesPagesDTO(query);
+    const totalPages = 9
   return <Pagination totalPages={totalPages} />;
 }
