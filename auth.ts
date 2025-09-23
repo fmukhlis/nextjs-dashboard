3b0c2b4f26827fa26aa8fc0d4c35b95c8fcfd96e
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-09-23 04:52:59 +0000

    feat: add Google Sign In method and protect dashboard route

--- auth.ts
+++ auth.ts
@@ -1,5 +1,14 @@
+import Google from "next-auth/providers/google";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Google],
+  callbacks: {
+    authorized: async ({ auth, request }) => {
+      if (request.nextUrl.pathname.startsWith("/dashboard")) {
+        return !!auth;
+      }
+      return true;
+    },
+  },
 });
