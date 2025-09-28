32261f0cb09c2445a41b6a511ea7d260ea185e68
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2025-09-28 08:05:41 +0000

    feat(seo): add metadata in invoices page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
 import "./globals.css";
+
 import { roboto } from "@/components/fonts";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
